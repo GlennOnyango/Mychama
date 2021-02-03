fc96893527d04eabe11b1d80e51217acc716c4db
Glenn <gonyango@>
2021-02-03 23:40:43 +0300

    all data

---
+++ app/src/main/java/com/example/mychama/Login.java
@@ -0,0 +1,35 @@
+package com.example.mychama;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Window window = getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.personalblue));
+        window.setNavigationBarColor(ContextCompat.getColor(this,R.color.personalblue));
+    }
+
+    public void moveFoward(View view) {
+        Intent i = new Intent(getApplicationContext(), Home.class);
+        startActivity(i);
+    }
+}
